2b7b99e02abf43e071885aa2f1ec39c7a5297f8a
SSweetKiller <T343$fh$9>
2020-03-11 23:03:41 +0200

    создание хэдера родительского класса

--- BOX/parameter.h
+++ BOX/parameter.h
@@ -1,5 +1,18 @@
 #pragma once
+#include<iostream>
+#include<string>
 class parameter
 {
+	public:
+		parameter() {}
+		parameter(int damage,);
+		~parameter();
+		
+		int damage;
+		
+		void set_damage(int damage);
+		int get_damage;
+protected:
+	int n_damage;
 };
 
